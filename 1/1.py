20de03a4c839f402fb6e05993849f3e358356bd1
Linus Lagergren <lagergrenlinus@gmail.com>
2020-12-01 12:44:32 +0100

    Solution to second part of first problem

--- 1/1.py
+++ 1/1.py
@@ -2,9 +2,18 @@ raw_data = open('input.txt').read().split('\n')
 raw_data.pop()
 data = list(map(lambda a: int(a), raw_data))
 
-def find_2020(data):
+def find_sum_of_two_numbers(data, sum):
     for x in data:
         for y in data:
-            if x + y == 2020:
+            if x + y == sum:
                 return x*y
-print(find_2020(data))
+
+def find_sum_of_three_numbers(data, sum):
+    for x in data:
+        for y in data:
+            for z in data:
+                if x + y + z == sum:
+                    return x*y*z
+
+print("Sum of two numbers:", find_sum_of_two_numbers(data, 2020))
+print("Sum of three numbers:", find_sum_of_three_numbers(data, 2020))
