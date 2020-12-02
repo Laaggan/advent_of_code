a359dfd4e94161c70b971fa49ac1661b1e193b46
Linus Lagergren <lagergrenlinus@gmail.com>
2020-12-02 12:55:45 +0100

    Solution to second part of day 2, where I also created a function for the first part

--- 2/2.py
+++ 2/2.py
@@ -6,8 +6,7 @@ data = open("input.txt").read()
 data = data.split("\n")
 data.pop()
 
-res = []
-for pw in data:
+def old_validation(pw):
     req, pw_in = pw.split(":")
     num, char = req.split(" ")
     num = list(map(lambda a: int(a), num.split("-")))
@@ -15,6 +14,35 @@ for pw in data:
     pw_count = pw_in.count(char)
 
     if num[0] <= pw_count <= num[1]:
-        res.append(pw_in)
+        return True, pw_in
+    else:
+        return False, pw_in
 
-print(len(res))
+def new_validation(pw):
+    req, pw_in = pw.split(":")
+    num, char = req.split(" ")
+    ind = list(map(lambda a: int(a), num.split("-")))
+
+    pw_count = pw_in.count(char)
+    cond1 = pw_in[ind[0]] == char and not pw_in[ind[1]] == char
+    cond2 = not pw_in[ind[0]] == char and pw_in[ind[1]] == char
+    if cond1 or cond2:
+        return True, pw_in
+    else:
+        return False, pw_in
+
+old_res = []
+new_res = []
+for pw in data:
+    is_correct, pw_in = new_validation(pw)
+
+    if is_correct:
+        new_res.append(pw_in)
+
+    is_correct, pw_in = old_validation(pw)
+
+    if is_correct:
+        old_res.append(pw_in)
+
+print("old:", len(old_res))
+print("new:", len(new_res))
