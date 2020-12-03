c1a93a1353b8f9f99f00a09cc117a86651e85dd0
laaggan <lagergrenlinus@gmail.com>
2020-12-03 09:48:42 +0100

    Solution to second part of day 3

--- 3/3.py
+++ 3/3.py
@@ -1,19 +1,28 @@
+from functools import reduce
 data = open('input.txt').read().split("\n")
 data.pop()
 
 width = len(data[0])
 x = 0
 y = 0
-hit_trees = 0
-while 0 < len(data):
-    if y >= len(data) - 1:
-        break
+curr_hit_trees = 0
+hit_trees = []
+xs = [1, 3, 5, 7, 1]
+ys = [1, 1, 1, 1, 2]
 
-    y += 1
-    x += 3
-    ind = x % width
-
-    if data[y][ind] == '#':
-        hit_trees += 1
+for x_i, y_i in zip(xs, ys):
+    while 0 < len(data):
+        if y >= len(data) - 1:
+            hit_trees.append(curr_hit_trees)
+            break
+        y += y_i
+        x += x_i
+        ind = x % width
+        if data[y][ind] == '#':
+            curr_hit_trees += 1
+    curr_hit_trees = 0
+    y, x = 0, 0
 
 print(hit_trees)
+result = reduce(lambda a, b: a*b, hit_trees)
+print(result)
