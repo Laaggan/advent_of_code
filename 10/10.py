2704c14323827b36772f8f6e0922cdd6f6dcb6fa
Linus Lagergren <lagergrenlinus@gmail.com>
2020-12-10 20:06:13 +0100

    Solution to first part of day 10

--- 10/10.py
+++ 10/10.py
@@ -1,19 +1,15 @@
 import copy
 
-data = list(map(lambda a: int(a), open('small_input.txt').read().split()))
+data = list(map(lambda a: int(a), open('input.txt').read().split()))
 
 temp_data = copy.deepcopy(data)
-valid_below = [3]
-valid_above = [1, 2, 3]
+start_valid = [1, 2, 3]
 print(temp_data)
 
-def set_of_valid_values(x, valid_below, valid_above):
-    new_valid_below = set(map(lambda a: x - a, valid_below))
-    new_valid_above = set(map(lambda a: x + a, valid_above))
-    return new_valid_below, new_valid_above
 
-_1jolt = 0
-_3jolt = 0
+def set_of_valid_values(_x, _valid):
+    new_valid = set(map(lambda a: _x + a, _valid))
+    return new_valid
 
 
 def remove_data_point(data, value):
@@ -21,26 +17,37 @@ def remove_data_point(data, value):
     data.pop(x_ind)
     return data
 
+_1jolt = 0
+_3jolt = 0
 
-for i in range(len(data)):
-    if i == 0:
-        x_curr = min(temp_data)
-        assert x_curr in valid_above, "No valid first value"
-        remove_data_point(temp_data, x_curr)
-        _1jolt += 1
+sol_found = False
+i = 0
+x_curr = 0
+for x in range(len(data)):
+    valid = set_of_valid_values(x_curr, start_valid)
+    candidates = valid.intersection(temp_data)
+
+    if len(candidates) == 1:
+        new_x = next(iter(candidates))
+        i_val = new_x - x_curr
+        x_curr = new_x
+        remove_data_point(temp_data, new_x)
+    elif len(candidates) > 1:
+        new_x = min(candidates)
+        i_val = new_x - x_curr
+        x_curr = new_x
+        remove_data_point(temp_data, new_x)
     else:
-        valid_above, valid_below = set_of_valid_values(x_curr, valid_below, valid_above)
-        for x in valid_above:
-            if x in temp_data:
-                x_curr = x
-                remove_data_point(temp_data, x_curr)
-                _1jolt += 1
-                break
-        for y in valid_below:
-            if y in temp_data:
-                x_curr = y
-                remove_data_point(temp_data, x_curr)
-                _3jolt += 1
-                break
+        raise Exception('something went wrong')
 
-print(_1jolt, _3jolt)
+    if i_val == 1:
+        _1jolt += 1
+    elif i_val == 3:
+        _3jolt += 1
+    else:
+        raise Exception('Something went wrong')
+
+print(temp_data)
+# Why do I under count 1?
+print(_1jolt, _3jolt + 1)
+print(_1jolt*(_3jolt + 1))
