32e3acde32cdfdeb4ec1b53244fbdddaa5250273
Linus Lagergren <lagergrenlinus@gmail.com>
2020-12-12 10:50:03 +0100

    Solution to first part of day 11

--- 11/11.py
+++ 11/11.py
@@ -1,27 +1,27 @@
 import copy
 
+
 def split_string(word):
     return [char for char in word]
 
-data = open('small_input.txt').read().split()
-data = [split_string(x) for x in data]
 
-# One can index into strings like below
-# print(data[0][1])
-
-h = len(data)
-w = len(data[0])
-
-print(h, w)
 def min_boundary(i):
     return max(0, i)
+
 
 def max_boundary(i, n):
     return min(i, n - 1)
 
+
+data = open('input.txt').read().split()
+data = [split_string(x) for x in data]
+
+h = len(data)
+w = len(data[0])
 temp_data = copy.deepcopy(data)
 result = copy.deepcopy(data)
 sol_found = False
+_iter = 0
 while not sol_found:
     alike = 0
     for i in range(h):
@@ -30,8 +30,8 @@ while not sol_found:
             h_i1, h_i2 = min_boundary(i-1), max_boundary(i+1, h)
             w_i1, w_i2 = min_boundary(j-1), max_boundary(j+1, w)
 
-            for k in range(h_i1, h_i2):
-                for l in range(w_i1, w_i2):
+            for k in range(h_i1, h_i2 + 1):
+                for l in range(w_i1, w_i2 + 1):
                     if k == i and l == j:
                         continue
                     else:
@@ -39,19 +39,30 @@ while not sol_found:
                             num_empty += 1
                         elif temp_data[k][l] == '#':
                             num_occupied += 1
+            if data[i][j] == '.':
+                continue
+            else:
+                if num_occupied == 0:
+                    result[i][j] = '#'
+                elif num_occupied >= 4:
+                    result[i][j] = 'L'
 
-            if num_occupied == 0:
-                result[i][j] = '#'
-            elif num_occupied >= 4:
-                result[i][j] = 'L'
-    temp_data = copy.deepcopy(result)
     for i in range(h):
         for j in range(w):
-            if data[i][j] == result[i][j]:
+            if temp_data[i][j] == result[i][j]:
                 alike += 1
-            print(result[i][j])
-        print("\n")
-    print("\n")
 
     if alike == w*h:
         sol_found = True
+
+    temp_data = copy.deepcopy(result)
+    print("iteration: ", _iter)
+    _iter += 1
+
+tot_num_occupied = 0
+for i in range(h):
+    for j in range(w):
+        if result[i][j] == '#':
+            tot_num_occupied += 1
+
+print("Number of occupied seats: ", tot_num_occupied)
