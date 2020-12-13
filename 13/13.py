3cc888aa537cdfedc4336206a84ceee7452840e2
Linus Lagergren <lagergrenlinus@gmail.com>
2020-12-13 15:13:09 +0100

    Solution to first part of day 13

--- 13/13.py
+++ 13/13.py
@@ -1,12 +1,12 @@
 from math import floor
-input_path = "small_input.txt"
+input_path = "input.txt"
 data = open(input_path).read().split("\n")
 data.pop()
 time_stamp = int(data[0])
 ids = data[1].split(",")
 ids = [int(id) for id in ids if id != 'x']
 
-
+'''
 print_str0 = "Timestamp, "
 for id in ids:
     print_str0 += " bus " + str(id) + ", "
@@ -18,3 +18,19 @@ for i in range(929, 950):
     filler = [i % id == 0 for id in ids]
     print_str1 = ("{:^"+str(pad)+"}")*cols
     print(print_str1.format(*[i, *filler]))
+'''
+
+
+def sol(ids, time_stamp):
+    sol_found = False
+    curr_time_stamp = time_stamp
+    while not sol_found:
+        for id in ids:
+            if curr_time_stamp % id == 0:
+                sol = id
+                return sol, curr_time_stamp
+        curr_time_stamp += 1
+
+
+this_sol = sol(ids, time_stamp)
+print(this_sol[0]*(this_sol[1]-time_stamp))
