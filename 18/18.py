c592cf949df1d89b03c1bd04e4509ba045f3f191
laaggan <lagergrenlinus@gmail.com>
2020-12-18 10:57:39 +0100

    Snapshot

--- 18/18.py
+++ 18/18.py
@@ -1,3 +1,4 @@
+import copy
 import re
 from functools import reduce
 
@@ -22,24 +23,43 @@ def sol_basic(_input):
             f = get_operator(c)
     return prev
 
+
+def find_first_right_bracket(_input):
+    i = 0
+    expr = "[)]"
+    while not bool(re.search(expr, _input[i])):
+        i += 1
+
+    if bool(re.search(expr, _input[i])):
+        return i
+    else:
+        raise Exception("Didn't find a right bracket")
+
+
 def sol(_input):
+    re_digit_without_left_bracket = "(?<!\()\d+"
+    if type(_input) == str:
+        _input = _input.split(" ")
     prev = []
     i = 0
     while i < len(_input):
         c = _input[i]
-        if bool(re.search("\d+", c)):
+        if bool(re.search(re_digit_without_left_bracket, c)):
             prev.append(int(c))
             if len(prev) >= 2:
                 prev = [reduce(f, prev)]
         elif bool(re.search("[*+]", c)):
             f = get_operator(c)
         elif bool(re.search("[(]", c)):
-            print(c, "left bracket")
-            local_ind = re.search("[)]", _input[i:]).start()
+            print(c[0])
+            #local_ind = re.search("[)]", _input[i:]).start()
+            local_ind = find_first_right_bracket(_input[i:])
             global_ind = i + local_ind
-            print(i, local_ind, global_ind)
-            new_exprs = _input[(i+1):global_ind]
+            #new_exprs = _input[(i+1):global_ind]
+            new_exprs = [c[1:], *_input[(i+1):global_ind], _input[global_ind][:-1]]
             sub_sol = sol(new_exprs)
+            new_input = copy.deepcopy(_input)
+            del new_input[i:(global_ind+1)]
             new_input = _input[:i] + str(sub_sol) + _input[(global_ind+1):]
             _input = new_input
         i += 1
@@ -48,3 +68,4 @@ def sol(_input):
 
 #print(sol_basic(basic_input))
 print(sol(small_input))
+#print(find_first_right_bracket(small_input.split()))
