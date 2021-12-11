cda6f4560d94a329c4ede0b52967346b81999ece
Linus Lagergren <linus.lagergren@spinit.se>
2021-12-11 14:47:09 +0100

    Solution to star 2 day 9

--- 9.py
+++ 9.py
@@ -1,38 +1,90 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 #file = open("input/test_input_day_9.txt").read()
 file = open("input/real_input_day_9.txt").read()
 data = np.array([[int(c) for c in row] for row in file.split("\n")])
 
-low_points = 0
-low_points_matrix = np.zeros(data.shape)
-for i in range(data.shape[0]):
-    for j in range(data.shape[1]):
-        surrounding_values = []
+def star_1(data):
+    low_points = 0
+    low_points_matrix = np.zeros(data.shape)
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            surrounding_values = []
 
-        if i > 0:
-            above = data[i - 1, j]
-            surrounding_values.append(above)
+            if i > 0:
+                above = data[i - 1, j]
+                surrounding_values.append(above)
 
-        if i < data.shape[0] - 1:
-            below = data[i + 1, j]
-            surrounding_values.append(below)
+            if i < data.shape[0] - 1:
+                surrounding_values.append([i + 1, j])
 
-        if j > 0:
-            left = data[i, j - 1]
-            surrounding_values.append(left)
+            if j > 0:
+                surrounding_values.append([i, j - 1])
 
-        if j < data.shape[1] - 1:
-            right = data[i, j + 1]
-            surrounding_values.append(right)
+            if j < data.shape[1] - 1:
+                surrounding_values.append([i, j + 1])
+    return surrounding_values
 
-        res = 0
-        for value in surrounding_values:
-            if data[i, j] < value:
-                res += 1
 
-        if res == len(surrounding_values):
-            low_points += data[i, j] + 1
-            low_points_matrix[i, j] = 1
+def get_neighborhood(i, j, data):
+    surrounding_indices = []
+    if i > 0:
+        surrounding_indices.append([i - 1, j])
 
-print(low_points)
+    if i < data.shape[0] - 1:
+        surrounding_indices.append([i + 1, j])
+
+    if j > 0:
+        surrounding_indices.append([i, j - 1])
+
+    if j < data.shape[1] - 1:
+        surrounding_indices.append([i, j + 1])
+    return surrounding_indices
+
+def calc_basin(point, data, mask, enumeration):
+    i, j = point
+    neighborhood = get_neighborhood(i, j, data)
+
+    for n_point in neighborhood:
+        i1, j1 = n_point
+        if data[i1, j1] == 9:
+            continue
+        elif data[i1, j1] > data[i, j]:
+            mask[i1, j1] = enumeration
+            calc_basin((i1, j1), data, mask, enumeration)
+
+def star_2(data):
+    mask = np.zeros(data.shape)
+    low_points = []
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            neighborhood = get_neighborhood(i, j, data)
+
+            larger_in_neighborhood = 0
+            for point in neighborhood:
+                i1, j1 = point
+                if data[i, j] < data[i1, j1]:
+                    larger_in_neighborhood += 1
+            if larger_in_neighborhood == len(neighborhood):
+                low_points.append((i, j))
+                mask[i, j] = 1
+    low_points = set(tuple(low_points))
+    for i, point in enumerate(low_points):
+        mask[point[0], point[1]] = i + 1
+        calc_basin(point, data, mask, i + 1)
+
+    result = []
+    for i in range(len(low_points)):
+        result.append(np.sum(np.sum(mask == i + 1)))
+
+    result.sort(reverse=True)
+    final_number = 1
+    for i in range(3):
+        final_number *= result[i]
+    return final_number
+
+
+
+
+print(star_2(data))
