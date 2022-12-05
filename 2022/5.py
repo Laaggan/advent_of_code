5f29268a798ea3c748adb011a0675b5a2a186b62
Linus Lagergren <linus.lagergren@spinit.se>
2022-12-05 22:11:52 +0100

    Small refactor and solution to part 2 day 5 2022

--- 2022/5.py
+++ 2022/5.py
@@ -7,40 +7,65 @@ def stack_pop(array):
 def stack_add(array, value):
     return [value, *array]
 
-f = open("data/5_real_data.txt")
-data = f.read()
+def parse_data():
+    f = open("data/5_real_data.txt")
+    data = f.read()
 
-data = data.split("\n\n")
-stack = data[0]
-instructions = data[1]
-stack = stack.split("\n")
-n = floor(len(stack[0])/4) + 1
-stack_structure = [[] for _ in range(n)]
-for x in stack[:-1]:
-    iter = 0
-    for i in range(n):
-        stack_value = x[iter:(iter + 4)]
-        iter += 4        
-        if (stack_value[1] == " "):
-            continue
-        else:
-            stack_structure[i].append(stack_value[1])
-instructions = instructions.split("\n")
+    data = data.split("\n\n")
+    stack = data[0]
+    instructions = data[1]
+    stack = stack.split("\n")
+    n = floor(len(stack[0])/4) + 1
+    stack_structure = [[] for _ in range(n)]
+    for x in stack[:-1]:
+        iter = 0
+        for i in range(n):
+            stack_value = x[iter:(iter + 4)]
+            iter += 4        
+            if (stack_value[1] == " "):
+                continue
+            else:
+                stack_structure[i].append(stack_value[1])
+    instructions = instructions.split("\n")
+    return stack_structure, instructions
 
-for instruction in instructions:
-    numbers = list(map(int, re.findall(r'(\d+)', instruction)))
-    
-    num = numbers[0]
-    i = numbers[1] - 1
-    j = numbers[2] - 1
-
-    new_i = stack_structure[i]
-    new_j = stack_structure[j]
-    for _ in range(num):
-        new_i, moved_value = stack_pop(new_i)
-        new_j = stack_add(new_j, moved_value)
-
-    stack_structure[i] = new_i
-    stack_structure[j] = new_j
+def solve_part_1():
+    stack_structure, instructions = parse_data()
+    for instruction in instructions:
+        numbers = list(map(int, re.findall(r'(\d+)', instruction)))
         
-print(*[x[0] for x in stack_structure])
+        num = numbers[0]
+        i = numbers[1] - 1
+        j = numbers[2] - 1
+
+        new_i = stack_structure[i]
+        new_j = stack_structure[j]
+        for _ in range(num):
+            new_i, moved_value = stack_pop(new_i)
+            new_j = stack_add(new_j, moved_value)
+
+        stack_structure[i] = new_i
+        stack_structure[j] = new_j
+            
+    print(*[x[0] for x in stack_structure])
+
+def solve_part_2():
+    stack_structure, instructions = parse_data()
+    for instruction in instructions:
+        numbers = list(map(int, re.findall(r'(\d+)', instruction)))
+        
+        num = numbers[0]
+        i = numbers[1] - 1
+        j = numbers[2] - 1
+
+        moved_containers = stack_structure[i][:num]
+        new_i = stack_structure[i][num:]
+        new_j = [*moved_containers, *stack_structure[j]]
+
+        stack_structure[i] = new_i
+        stack_structure[j] = new_j
+            
+    print(*[x[0] for x in stack_structure])
+
+#solve_part_1()
+solve_part_2()
