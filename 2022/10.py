72e986149c5da44746e49293973dc6893df1366a
Linus Lagergren <linus.lagergren@spinit.se>
2022-12-14 17:38:26 +0100

    solution to part 2 day 10 2022

--- 2022/10.py
+++ 2022/10.py
@@ -1,8 +1,5 @@
 data = open("data/10_small_data.txt").read()
 data = open("data/10_real_data.txt").read()
-# data = """noop
-# addx 3
-# addx -5"""
 data = data.split("\n")
 
 NUM_CYCLES = {
@@ -10,27 +7,65 @@ NUM_CYCLES = {
     "noop": 1
 }
 
-registry = 1
-clock_cycles = 0
+def solution_part_1():
+    registry = 1
+    clock_cycles = 0
 
-result = []
-for op in data:
-    if op == "noop":
-        value = None
+    result = []
+    for op in data:
+        if op == "noop":
+            value = None
+        else:
+            op, value = op.split(" ")
+            value = int(value)
+        
+        for i in range(NUM_CYCLES[op]):
+            clock_cycles += 1
+
+            if (clock_cycles == 20):
+                result.append(clock_cycles * registry)
+            elif ((clock_cycles + 20) % 40 == 0):
+                result.append(clock_cycles * registry)
+        if (value):
+            registry += value
+        elif (clock_cycles > 220):
+            break
+
+    print(sum(result))
+
+def logic_sprite_visibility(sprite_pos, clock_cycles):
+    x = clock_cycles % 40
+    if sprite_pos == x:
+        return "#"
+    elif sprite_pos == x - 1:
+        return "#"
+    elif sprite_pos == x + 1:
+        return "#"
     else:
-        op, value = op.split(" ")
-        value = int(value)
-    
-    for i in range(NUM_CYCLES[op]):
-        clock_cycles += 1
+        return "."
 
-        if (clock_cycles == 20):
-            result.append(clock_cycles * registry)
-        elif ((clock_cycles + 20) % 40 == 0):
-            result.append(clock_cycles * registry)
-    if (value):
-        registry += value
-    elif (clock_cycles > 220):
-        break
+def solution_part_2():
+    registry = 1
+    clock_cycles = 0
 
-print(sum(result))
+    result = ""
+    current_crt_row = ""
+    for op in data:
+        if op == "noop":
+            value = None
+        else:
+            op, value = op.split(" ")
+            value = int(value)
+        
+        for _ in range(NUM_CYCLES[op]):
+            current_crt_row += logic_sprite_visibility(registry, clock_cycles)
+            clock_cycles += 1
+            if (clock_cycles % 40 == 0):
+                result += current_crt_row + "\n"
+                current_crt_row = ""
+        if (value):
+            registry += value
+
+    print(result)
+
+solution_part_2()
