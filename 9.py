aefc54d9c71a273389c684a0f7e8cc1230437833
Linus Lagergren <linus.lagergren@spinit.se>
2022-12-14 16:19:13 +0100

    Solution to part 2 day 9 2022

--- 9.py
+++ 9.py
@@ -1,5 +1,3 @@
-from math import floor
-import numpy as np
 data = """R 4
 U 4
 L 3
@@ -8,6 +6,15 @@ R 4
 D 1
 L 5
 R 2"""
+
+data = """R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20"""
 data = open("C:/Users/llag/source/repos/advent_of_code/2022/data/9_real_data.txt").read()
 
 data = [[x.split(" ")[0], int(x.split(" ")[1])] for x in data.split("\n")]
@@ -23,10 +30,6 @@ def moving_logic_for_head(x, y, direction):
         return (x - 1, y)
 
 def moving_logic_for_tail(h_x, h_y, t_x, t_y):
-    # If position is same do nothing
-    # if (h_x == h_y and h_y == h_y):
-    #     return t_x, t_y
-
     # If x or y is equal move only in one direction
     if (h_x == t_x and abs(h_y - t_y) > 1):
         if (h_y > t_y):
@@ -54,22 +57,40 @@ def moving_logic_for_tail(h_x, h_y, t_x, t_y):
     
     return t_x, t_y
 
+def solve_part_1():
+    current_direction = None
+    x_h, y_h, x_t, y_t = 0, 0, 0, 0
 
-current_direction = None
-x_h, y_h, x_t, y_t = 0, 0, 0, 0
+    has_been = set()
+    for direction, count in data:
+        current_direction = direction
+        for i in range(count):
+            x_h, y_h = moving_logic_for_head(x_h, y_h, current_direction)
+            x_t, y_t = moving_logic_for_tail(x_h, y_h, x_t, y_t)
+            has_been.add((x_t, y_t))
+        
+    print(len(has_been))
 
-has_been = set()
-for direction, count in data:
-    current_direction = direction
-    # print(direction, count)
-    # print('------')
-    for i in range(count):
-        x_h, y_h = moving_logic_for_head(x_h, y_h, current_direction)
-        x_t, y_t = moving_logic_for_tail(x_h, y_h, x_t, y_t)
-        # print('T', x_t, y_t)
-        # print('H', x_h, y_h)
-        # print('------')
-        has_been.add((x_t, y_t))
+def solve_part_2():
+    tail_size = 9
+    current_direction = None
+    x_h, y_h = 0, 0
+    tail = [(0, 0) for _ in range(tail_size)]
+    has_been = set()
     
-print(len(has_been))
+    for direction, count in data:
+        current_direction = direction
+        for i in range(count):
+            x_h, y_h = moving_logic_for_head(x_h, y_h, current_direction)
+            for i, (x_t_curr, y_t_curr) in enumerate(tail):
+                if i == 0:
+                    x_t, y_t = moving_logic_for_tail(x_h, y_h, x_t_curr, y_t_curr)
+                else:
+                    x_t_before, y_t_before = tail[i-1]
+                    x_t, y_t = moving_logic_for_tail(x_t_before, y_t_before, x_t_curr, y_t_curr)
+                tail[i] = (x_t, y_t)
+                if i == len(tail) - 1:
+                    has_been.add((x_t, y_t))
+    print(len(has_been))
     
+solve_part_2()
