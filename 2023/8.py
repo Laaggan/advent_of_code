7fb9fc704529e6f687ea3356c27f24bb24587164
Linus Lagergren <linus.lagergren@spinit.se>
2023-12-24 16:13:06 +0100

    Solution to part 2 day 8 2023 using Euclids algorithm

--- 2023/8.py
+++ 2023/8.py
@@ -58,28 +58,56 @@ def find_all_starting_positions(dict_positions: dict):
             result.append(key)
     return result
 
-def check_end_condition(current_positions):
-    for position in current_positions:
-        if position[2] != "Z":
-            return True
+def check_end_condition(current_position):    
+    if current_position[2] != "Z":
+        return True
     return False
+
+def solve_sub_problem(start_position):
+    current_position = start_position
+    i = 0
+    while check_end_condition(current_position):
+        current_instruction = instructions[i % len(instructions)]
+        
+        if current_instruction == "R":
+            current_position = dict_positions[current_position][1]
+        else:
+            current_position = dict_positions[current_position][0]
+        i += 1
+    return i
+
+#TODO: Put in library file?
+def gcd(num1, num2):
+    if num1 > num2:
+        x = num1
+        d = num2
+    else:
+        x = num2
+        d = num1
+    
+    remainder = x % d
+    
+    if remainder == 0:
+        return d
+    else:
+        return gcd(d, remainder)
+            
+
+def lcm_of_array(arr, idx):
+    if (idx == len(arr) - 1):
+        return arr[idx]
+    a = arr[idx]
+    b = lcm_of_array(arr, idx + 1)
+    return int(a*b/gcd(a, b))
 
 def solution_part_2():
     current_positions = find_all_starting_positions(dict_positions)
-    i = 0
-    while check_end_condition(current_positions):
-        if (i % 100 == 0):
-            print("step:", i)
-        current_instruction = instructions[i % len(instructions)]
-        
-        temp_current_positions = []
-        for current_position in current_positions:
-            if current_instruction == "R":
-                temp_current_positions.append(dict_positions[current_position][1])
-            else:
-                temp_current_positions.append(dict_positions[current_position][0])
-        current_positions = temp_current_positions
-        i += 1
-    print(i)
+    sub_solutions = []
+    for current_position in current_positions:
+        sub_solution = solve_sub_problem(current_position)
+        sub_solutions.append(sub_solution)
+    
+    result = lcm_of_array(sub_solutions, 0)
+    return result
 
-solution_part_2()
+print(solution_part_2())
