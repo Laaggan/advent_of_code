904af4b632b956a98ee94cdcfa7cf4554bcd741f
Linus Lagergren <30722015+Laaggan@users.noreply.github.com>
2024-12-29 13:56:49 +0100

    Solution to part 2 day 8 2024

--- 2024/8.py
+++ 2024/8.py
@@ -10,25 +10,52 @@ for i, row in enumerate(data):
         if c != ".":
             frequency_positions[c].append((i,j))
 
-anti_nodes = set()
-for frequency in frequency_positions.keys():
-    positions = frequency_positions[frequency]
-    for i, position_1 in enumerate(positions):
-        for position_2 in positions:
-            if position_1 == position_2:
-                continue
+def solution_1(data):
+    anti_nodes = set()
+    for frequency in frequency_positions.keys():
+        positions = frequency_positions[frequency]
+        for i, position_1 in enumerate(positions):
+            for position_2 in positions:
+                if position_1 == position_2:
+                    continue
 
-            diff = (position_1[0]-position_2[0], position_1[1]-position_2[1])
-            
-            anti_node_pos_1 = (position_1[0] + diff[0], position_1[1] + diff[1])
-            if anti_node_pos_1[0] >= 0 and anti_node_pos_1[0] < len(data) and anti_node_pos_1[1] >= 0 and anti_node_pos_1[1] < len(data[0]):
-                anti_nodes.add(anti_node_pos_1)
-            
-            anti_node_pos_2 = (position_2[0] - diff[0], position_2[1] - diff[1])
-            if anti_node_pos_2[0] >= 0 and anti_node_pos_2[0] < len(data) and anti_node_pos_2[1] >= 0 and anti_node_pos_2[1] < len(data[0]):
-                anti_nodes.add(anti_node_pos_2)
+                diff = (position_1[0]-position_2[0], position_1[1]-position_2[1])
+                
+                anti_node_pos_1 = (position_1[0] + diff[0], position_1[1] + diff[1])
+                if anti_node_pos_1[0] >= 0 and anti_node_pos_1[0] < len(data) and anti_node_pos_1[1] >= 0 and anti_node_pos_1[1] < len(data[0]):
+                    anti_nodes.add(anti_node_pos_1)
+                
+                anti_node_pos_2 = (position_2[0] - diff[0], position_2[1] - diff[1])
+                if anti_node_pos_2[0] >= 0 and anti_node_pos_2[0] < len(data) and anti_node_pos_2[1] >= 0 and anti_node_pos_2[1] < len(data[0]):
+                    anti_nodes.add(anti_node_pos_2)
+    return anti_nodes
 
-print(len(anti_nodes))
+def solution_2(data):
+    anti_nodes = set()
+    for frequency in frequency_positions.keys():
+        positions = frequency_positions[frequency]
+        for i, position_1 in enumerate(positions):
+            anti_nodes.add(position_1)
+            for position_2 in positions:
+                if position_1 == position_2:
+                    continue
+
+                diff = (position_1[0]-position_2[0], position_1[1]-position_2[1])
+                
+                anti_node_pos_1 = (position_1[0] + diff[0], position_1[1] + diff[1])
+                while anti_node_pos_1[0] >= 0 and anti_node_pos_1[0] < len(data) and anti_node_pos_1[1] >= 0 and anti_node_pos_1[1] < len(data[0]):
+                    anti_nodes.add(anti_node_pos_1)
+                    anti_node_pos_1 = (anti_node_pos_1[0] + diff[0], anti_node_pos_1[1] + diff[1])
+                
+                anti_node_pos_2 = (position_2[0] - diff[0], position_2[1] - diff[1])
+                while anti_node_pos_2[0] >= 0 and anti_node_pos_2[0] < len(data) and anti_node_pos_2[1] >= 0 and anti_node_pos_2[1] < len(data[0]):
+                    anti_nodes.add(anti_node_pos_2)
+                    anti_node_pos_2 = (anti_node_pos_2[0] - diff[0], anti_node_pos_2[1] - diff[1])
+    return anti_nodes
+
+print(len(solution_1(data)))
+anti_nodes = solution_2(data)
+print(len(anti_nodes)) # 901 is too low
 
 # mod_data = deepcopy(data)
 # for node in anti_nodes:
@@ -37,6 +64,3 @@ print(len(anti_nodes))
 
 # for row in mod_data:
 #     print("".join(row))
-
-
-        
