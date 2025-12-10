f6d265abdf8b49d26e61f839e1f4b71969db20da
Lagergren Linus (2) (Consultant) <linus.lagergren.2@consultant.volvo.com>
2025-12-10 10:20:47 +0900

    Solution to day 2 part 1

---
+++ 2025/2.py
@@ -0,0 +1,21 @@
+data = '''11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124'''
+
+with open("2025/data/2.txt", 'r') as file:
+    data = file.read()
+
+data = [list(map(int, x.split("-"))) for x in data.split(",")]
+
+result = []
+for pair in data:
+    for i in range(pair[0], pair[1] + 1):
+        c_str = str(i)
+        # then we need to identify mirrored sequences
+        # first check is that the number contains a even number of digits
+        if len(c_str) % 2 != 0:
+            continue
+         
+        middle = len(c_str) // 2
+        if c_str[:middle] == c_str[middle:]:
+            result.append(i)
+
+print(sum(result))
