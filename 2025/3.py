8a1d2e48a02fb9cf9c66cb17d79e88508d00d9cc
Lagergren Linus (2) (Consultant) <linus.lagergren.2@consultant.volvo.com>
2025-12-11 14:48:44 +0900

    Solution to 2025 day 3 part 2

--- 2025/3.py
+++ 2025/3.py
@@ -8,27 +8,36 @@ with open("2025/data/3.txt") as file:
 
 data = data.split("\n")
 
-result = []
-for row in data: 
-    # first_max = 0
-    # second_max = 0
-    # for i, c in enumerate(row):
-    #     if first_max < int(c):
-    #         first_max = int(c)
-    #         first_max_i = i
-    
-    # for i, c in enumerate(row[(first_max_i+1):]):
-    #     if second_max < int(c):
-    #         second_max = int(c)
-    
-    # result.append(int(str(first_max) + str(second_max)))
-    all_comb = []
-    for i, c1 in enumerate(row):
-        for j in range(i+1, len(row)):
-            c2 = row[j]
-            all_comb.append(int(c1 + c2))
-    result.append(max(all_comb))
+row_result = []
+# for row in data: 
+#     all_comb = []
+#     for i, c1 in enumerate(row):
+#         for j in range(i+1, len(row)):
+#             c2 = row[j]
+#             all_comb.append(int(c1 + c2))
+#     result.append(max(all_comb))
+
+result=[]
+for row in data:
+    remaining_picks = 12
+    most_recently_used_index = 0
+    row_result = ""
+    while remaining_picks > 0:
+        last_allowed_index = len(row) - remaining_picks + 1
+        max_in_range = 0
+        for i in range(most_recently_used_index, last_allowed_index):
+            if int(max_in_range) < int(row[i]):
+                max_in_range = row[i]
+                max_index = i
+        # print("val:", max_in_range, "i:", max_index)
+        row_result += max_in_range
+        most_recently_used_index = max_index + 1
+        remaining_picks -= 1
+    result.append(row_result)
 
 print(result)
-print(sum(result))
+print(sum(map(int, result)))
+
+# print(result)
+# print(sum(result))
     
